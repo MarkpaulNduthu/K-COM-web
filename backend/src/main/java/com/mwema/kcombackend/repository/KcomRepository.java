5d76c14e22b66f9b5e56b36123d0aae1c2b71120
MarkpaulNduthu <paulmarkmwema@gmail.com>
2025-07-31 23:44:14 +0300

    setup clerk auth

---
+++ backend/src/main/java/com/mwema/kcombackend/repository/KcomRepository.java
@@ -0,0 +1,8 @@
+package com.mwema.kcombackend.repository;
+
+import com.mwema.kcombackend.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KcomRepository extends JpaRepository<Product,Long> {}
